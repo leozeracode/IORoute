1721e8f6425a3b6b4ebb816e011d9758462e542b
Leonardo Viana <leozeracode@gmail.com>
2024-05-14 16:43:58 -0300

    chore: add api layer and config

---
+++ src/IORoute.API/Program.cs
@@ -0,0 +1,39 @@
+using IORoute.Infra;
+using IORoute.Domain.Usecases;
+using IORoute.Data.Usecases;
+using IORoute.Infra.Persistence;
+using IORoute.API.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddScoped<IGetBestRoute, GetBestRoute>();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MigrateDataBase<RouteDbContext>((context, service) =>
+{
+    var logger = service.GetService<ILogger<RouteContextSeed>>();
+    RouteContextSeed.SeedAsync(context, logger).Wait();
+});
+
+
+app.Run();
+
+
