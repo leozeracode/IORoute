1cc5c71792a7550368d09a063ad0a3aed6dc3994
Leonardo Viana <leozeracode@gmail.com>
2024-05-14 15:49:35 -0300

    test: add load route repository test

---
+++ test/IORoute.Test/Infra/Repositories/LoadRoutesRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using IORoute.Domain.Models.Entities;
+using IORoute.Infra.Persistence;
+using IORoute.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace IORoute.Test.Infra.Repositories
+{
+    public class LoadRoutesRepositoryTests
+    {
+        [Fact]
+        public void LoadRoutes_ShouldReturn_Routes()
+        {
+            var options = new DbContextOptionsBuilder<RouteContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) 
+                .Options;
+
+            using (var context = new RouteContext(options))
+            {
+                context.Routes.Add(new Route { Origin = "GRU", Destination = "BRC", Cost = 10 });
+                context.Routes.Add(new Route { Origin = "BRC", Destination = "SCL", Cost = 5 });
+                context.Routes.Add(new Route { Origin = "GRU", Destination = "CDG", Cost = 75 });
+                context.Routes.Add(new Route { Origin = "GRU", Destination = "SCL", Cost = 20 });
+                context.Routes.Add(new Route { Origin = "GRU", Destination = "ORL", Cost = 56 });
+                context.Routes.Add(new Route { Origin = "ORL", Destination = "CDG", Cost = 5 });
+                context.Routes.Add(new Route { Origin = "SCL", Destination = "ORL", Cost = 20 });
+
+                context.SaveChanges();
+            }
+
+            using (var context = new RouteContext(options))
+            {
+                var repository = new RoutesRepository(context);
+                var routes = repository.LoadRoutes();
+
+                Assert.NotNull(routes);
+                Assert.Equal(7, routes.Count()); 
+            }
+        }
+    }
+}
