17ecec3395cc107e61791ec857b4e95dba6ffa8a
Leonardo Viana <leozeracode@gmail.com>
2024-05-14 15:49:18 -0300

    feat: add route repositories

---
+++ src/IORoute.Infra/InfraServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using IORoute.Data.Protocols;
+using IORoute.Infra.Persistence;
+using IORoute.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IORoute.Infra
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<RouteContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<RouteContext>();
+
+            services.AddScoped<ILoadRoutesRepository, RoutesRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ src/IORoute.Infra/Repositories/RoutesRepository.cs
@@ -0,0 +1,27 @@
+﻿using IORoute.Data.Protocols;
+using IORoute.Domain.Models.DTO;
+using IORoute.Infra.Persistence;
+
+namespace IORoute.Infra.Repositories
+{
+    public class RoutesRepository : ILoadRoutesRepository
+    {
+        private readonly RouteContext _dbContext;
+
+        public RoutesRepository(RouteContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<RouteModel> LoadRoutes()
+        {
+            var routes = _dbContext.Routes.ToList();
+            var routeModels = routes.Select(route => new RouteModel
+            {
+                Origin = route.Origin,
+                Destination = route.Destination,
+                Cost = route.Cost
+            });
+            return routeModels;
+        }
+    }
+}
